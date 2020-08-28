fba8a80a7c97b275fb1a1afee43619f8561c8d4f
AlexV2204 <alexandre.vilbert1@gmail.com>
2020-08-28 11:15:56 +0200

    ajout de la pop up et de son script sur le projet

--- script.js
+++ script.js
@@ -16,3 +16,13 @@ document.onkeydown = function(e){
     user_keys = [];
   }
 }
+
+// script pour ouvrir la pop up selon l'id du profil
+function info_page(id) {
+  id = id+"ip";
+  var elmt = document.getElementById(id);
+  var blur = document.getElementById("blur");
+  
+  blur.style.visibility="visible";
+  elmt.style.visibility="visible";
+}
